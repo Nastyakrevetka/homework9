ed6cde5005a66b1adf4bead84947a872cefade8c
Nastya_krevtka <nastyamkala1278@gmail.com>
2023-01-07 23:11:35 +0300

    задача1 числа от n до 1

---
+++ ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте значение N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void PrintString(int n, int count)
+{
+  if (count > n) return;
+  PrintString(n, count + 1);
+  Console.Write(count + "; ");
+}
+
+int PrintNumber(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+Console.Clear();
+int n = PrintNumber("Введите n: ");
+int count = 2;
+PrintString(n, count);
+Console.Write(1);
+
+
