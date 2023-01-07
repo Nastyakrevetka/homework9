a04254d2c8b1b55a0e1bc107fb9179d865bf4d02
Nastya_krevtka <nastyamkala1278@gmail.com>
2023-01-07 23:20:32 +0300

    задача2 сумма от m до n

---
+++ ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Clear();
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма элементов= {summ} ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+
+int PrintNumber(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+int m = PrintNumber("Введите m: ");
+int n = PrintNumber("Введите n: ");
+int count = m;
+if (m > n) 
+{
+  m = n; 
+  n = count;
+}
+
+PrintSumm(m, n, count=0);
